3f1cd999f2adc9a3de85a613fdbdbe7ee0b85475
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-02-24 13:09:53 +0000

    change routes, add bookmark form

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,20 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+
   def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
   end
 
   def create
   end
 
   def destroy
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,16 +4,27 @@ class ListsController < ApplicationController
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @new_list = List.new(list_params)
+    @new_list.save
+
+    redirect_to lists_path
   end
 
   def show
     bookmarks = Bookmark.all
+    @list_id = params[:id]
     @movies = bookmarks.select do |bookmark|
       bookmark.list["id"] == params[:id].to_i
     end
-    
+  end
+
+  private 
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
